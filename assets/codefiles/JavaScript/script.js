2806e2cdfbf6de0fb28d72363819fbe9e42a4a2a
Ayush <85835266+ayush-alam@users.noreply.github.com>
2021-07-15 15:27:44 +0530

    Add files via upload

---
+++ assets/codefiles/JavaScript/script.js
@@ -0,0 +1,35 @@
+// The WebPage is developed by Ayush Alam, Apple On Head
+
+function buttonchange(){
+    document.getElementById('butt1').style.backgroundColor = 'darkorange';
+    document.getElementById('butt1').style.border = 'black solid';
+}
+
+function buttontwo(){
+    document.getElementById('butt1').style.backgroundColor = 'orange';
+    document.getElementById('butt1').style.border = 'none';
+}
+
+function othbutton(){
+    document.getElementById('butt2').style.backgroundColor = 'darkorange';
+    document.getElementById('butt2').style.border = 'black solid';
+}
+
+function othbuttontwo(){
+    document.getElementById('butt2').style.backgroundColor = 'orange';
+    document.getElementById('butt2').style.border = 'none';
+}
+
+function funbutton(){
+    document.getElementById('fun').style.backgroundColor = 'darkorange';
+    document.getElementById('fun').style.border = 'black solid';
+}
+
+function funbuttontwo(){
+    document.getElementById('fun').style.backgroundColor = 'orange';
+    document.getElementById('fun').style.border = 'none';
+}
+
+function fun(){
+    alert('No fun allowed ! But go under an apple tree NOW !')
+}
---
+++ assets/codefiles/Others/README.md
@@ -0,0 +1,5 @@
+<!-- The WebPage is developed by Ayush Alam, Apple On Head -->
+
+# Apple On Head
+Sooo you guys! Wassup! This is me, Ayush and this is gonna be our official Webpage!
+Go [there](https://apple-on-head.github.io/aoh/)
